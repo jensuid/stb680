df82c395c40ab6b78f1a023c21c1c7ef439b444b
Juni Aji <aji.daryanto@gmail.com>
2020-06-28 17:39:30 +0700

    edit lb script

--- installLB.sh
+++ installLB.sh
@@ -8,6 +8,8 @@ mkdir /sdcard/tempLB/
 curl -L https://www.dropbox.com/s/lizqmp4yas9yhje/AppDrawer.apk?dl=0 > /sdcard/tempLB/AppDrawer.apk 
 curl -L https://www.dropbox.com/s/9mpev6cz7cnrgks/LeanbackLauncher.apk?dl=0 > /sdcard/tempLB/LeanbackLauncher.apk 
 
+echo "----------------------"
+echo "Installing ..........."
 #install apk
 #install leanback
 cp /sdcard/tempLB/LeanbackLauncher.apk /system/priv-app/
@@ -18,7 +20,11 @@ mkdir /system/app/AppDrawer/
 cp /sdcard/tempLB/AppDrawer.apk /system/app/AppDrawer/
 chmod 644 /system/app/AppDrawer/AppDrawer.apk
 
+echo ""
+echo "Clearing .........."
 rm -rf /sdcard/tempLB/
 #clear
 #reboot
+echo ""
+echo "rebooting ....."
 reboot
