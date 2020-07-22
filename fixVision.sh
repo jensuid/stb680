dada836f844ad67153e72f635ac9b6f9308333f3
Juni Aji <aji.daryanto@gmail.com>
2020-07-22 20:37:08 +0700

    add fix vision hg

--- fixVision.sh
+++ fixVision.sh
@@ -3,7 +3,8 @@ mount -o rw,remount /system
 
 echo " Fixing Error Playback Aplikasi Vision+"
 sed -i '/ro.product.device=pulpstone_hg680p/ c ro.product.device=p212' /system/build.prop
-#clear
+#clear oem crypto
+mv /vendor/lib/liboemcrypto.so /vendor/lib/liboemcrypto.BYE
 #reboot
 echo ""
 echo "rebooting ....."
